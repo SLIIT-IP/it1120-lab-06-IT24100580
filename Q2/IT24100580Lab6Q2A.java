32b61886f10d05bd55e730ff153adb8b23a51b57
IT24100580 <it24100580@my.sliit.lk>
2024-08-28 11:57:58 +0530

    add IT24100580Lab6Q2A.java to Q2A

---
+++ Q2/IT24100580Lab6Q2A.java
@@ -0,0 +1,17 @@
+
+  public class IT24100580Lab6Q2A{
+     public static void main(String[] args){
+     
+
+       int number=1;
+       
+
+
+      while( number<=10) {
+        System.out.print(number + " ");
+        number++;
+      
+      }
+   }
+
+}
